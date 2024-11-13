dbe089e9fdccdb2133b27317fc6797b54576a6af
@jjspscl <171547923+nexplore-jjspscl@users.noreply.github.com>
2024-11-13 12:26:24 +0800

    🧹 CLEANUP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+    })    
+  ],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: '@jjspscl/react-onedrive',
+      fileName: (format) => `react-onedrive.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      }
+    }
+  }
 })
